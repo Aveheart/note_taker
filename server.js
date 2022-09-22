1a3d00982492dca7420dac6cc29f161b95557492
Averi <averi.sherman@gmail.com>
2022-09-21 21:10:36 -0400

    added middleware

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-const express = require('express');
+const express = require("express");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3000 ; 
 
-
-// Parse URL encoded & JSON
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Host public folder
-app.use(express.static('public'));
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
-});
