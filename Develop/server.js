241657c41695f645cf11070989d25e06c1c9ea09
Averi <averi.sherman@gmail.com>
2022-09-21 21:06:20 -0400

    restarted/fixed file structure

--- Develop/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
