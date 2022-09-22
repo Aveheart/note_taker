f1648108ffd68f2df340f9dca8147f0b712a820f
Averi <averi.sherman@gmail.com>
2022-09-21 21:26:42 -0400

    Get routes for the html

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 var notesDB = require("./db/db.json");
 
-// middleware
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
+// GET ROUTES
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 
