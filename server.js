0de27896a6356c67d2e6e2a307318e5d8652c93c
Averi <averi.sherman@gmail.com>
2022-09-21 21:31:51 -0400

    get route for api

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// GET ROUTES
+// GET ROUTES FOR HTML
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+
+// GET ROUTE FOR API
+app.get("/api/notes", (req,res) => { res.json(notesDB)});
+
+// GET ROUTE FOR FRONTPAGE
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
